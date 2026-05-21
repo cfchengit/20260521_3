a81d6ca004542cbb3bd26c941f71dd5b9d3fe1ae
cfchen_tku <cfchen@mail.tku.edu.tw>
2026-05-21 23:26:36 +0800

    1.01

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/quake-tracker/'   // ← 改成你的 GitHub Repository 名稱
+  base: '/20260521_3/'   // ← 改成你的 GitHub Repository 名稱
 })
